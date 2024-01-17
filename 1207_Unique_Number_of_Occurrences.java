bbc7d8d8f8eb0d28d1d6df1786fc0d6efda8f88f
Ritik Yadav <ritikyadav2325@gmail.com>
2024-01-17 19:51:36 +0530

    update

---
+++ 1207_Unique_Number_of_Occurrences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        HashMap<Integer,Integer> res=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> ent:map.entrySet()){
+            if(res.containsKey(ent.getValue())){
+                return false;
+            }
+            res.put(ent.getValue(),0);
+        }
+        return true;
+    }
+}
