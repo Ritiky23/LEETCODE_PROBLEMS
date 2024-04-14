b52fe0e5aa4ccdc9e008c176c4001b07218adcb6
Ritik Yadav <120368901+Ritiky23@users.noreply.github.com>
2024-04-14 18:24:20 +0530

    Create Sum_of_Left_leaves.java
    update

---
+++ Sum_of_Left_leaves.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+public int sumOfLeftLeavesHelper(TreeNode root,boolean isLeft) {
+        if(root==null)
+            return 0;
+        if(root.left==null && root.right==null && isLeft==true)
+         return root.val;
+        return sumOfLeftLeavesHelper(root.left,true)+sumOfLeftLeavesHelper(root.right,false);
+    }
+    public int sumOfLeftLeaves(TreeNode root) {
+        return sumOfLeftLeavesHelper(root,false); 
+    }}
