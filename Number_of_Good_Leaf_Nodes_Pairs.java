1e9fb5185035f1f2173b1fe502d940b800d19ed6
Ritik Yadav <120368901+Ritiky23@users.noreply.github.com>
2024-07-18 23:39:24 +0530

    Create Number_of_Good_Leaf_Nodes_Pairs.java

---
+++ Number_of_Good_Leaf_Nodes_Pairs.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Integer result;
+    public int countPairs(TreeNode root, int distance) {
+        result=0;
+        countPairsHelper(root,distance);
+        return result;
+    }
+    private int[] countPairsHelper(TreeNode root,int distance){
+        int[] arr = new int[10];
+        if(root==null){
+            return arr;
+        }
+        else if(root.left==null && root.right==null){
+            arr[0]=1;
+            return arr;
+        }
+        int[] leftArr = countPairsHelper(root.left,distance);
+        int[] rightArr = countPairsHelper(root.right,distance);
+        for(int loop1=0;loop1<10;loop1++){
+            for(int loop2=0;loop2<10;loop2++){
+                if(loop1+loop2+2<=distance){
+                    result += (leftArr[loop1]*rightArr[loop2]);
+                }
+            }
+        }
+        for(int loop=0;loop<9;loop++){
+            arr[loop+1]= leftArr[loop]+rightArr[loop];
+        }
+        return arr;
+    }
+}
